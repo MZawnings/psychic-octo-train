761ba208b2b4037eebcbde9b91c5338cebcd144a
MZ_Moola <96755127+MZawnings@users.noreply.github.com>
2023-04-17 21:10:39 +0200

    Initialize develop_7

---
+++ test/libsolidity/semanticTests/reverts/invalid_enum_as_external_arg.sol
@@ -0,0 +1,22 @@
+contract C {
+    enum X {A, B}
+
+    function tested(X x) public returns (uint256) {
+        return 1;
+    }
+
+    function test() public returns (uint256) {
+        X garbled;
+
+        assembly {
+            garbled := 5
+        }
+
+        return this.tested(garbled);
+    }
+}
+// ====
+// compileViaYul: also
+// compileToEwasm: also
+// ----
+// test() -> FAILURE # should throw #
