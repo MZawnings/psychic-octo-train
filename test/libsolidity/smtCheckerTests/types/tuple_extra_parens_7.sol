0a0b59b4d7f34e0f504d476cdc384d92ada69fdb
MZ_Moola <96755127+MZawnings@users.noreply.github.com>
2023-04-17 21:30:37 +0200

    Initialize asm-immutable-refs-mathias

---
+++ test/libsolidity/smtCheckerTests/types/tuple_extra_parens_7.sol
@@ -0,0 +1,14 @@
+contract C {
+	function g() internal pure returns (uint, uint) {
+		return (2, 3);
+	}
+	function f() public {
+		(address(1).call(""));
+		(uint x, uint y) = ((g()));
+		assert(x == 2);
+		assert(y == 3);
+	}
+}
+// ====
+// SMTEngine: all
+// ----
