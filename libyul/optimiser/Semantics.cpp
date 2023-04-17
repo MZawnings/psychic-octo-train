ab4312bc550edcbb111c944e67b0482423a91a47
MZ_Moola <96755127+MZawnings@users.noreply.github.com>
2023-04-17 21:02:21 +0200

    Initialize makeMSizeConstant

---
+++ libyul/optimiser/Semantics.cpp
@@ -0,0 +1,126 @@
+/*(
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * Specific AST walkers that collect semantical facts.
+ */
+
+#include <libyul/optimiser/Semantics.h>
+
+#include <libyul/Exceptions.h>
+#include <libyul/AsmData.h>
+#include <libyul/Dialect.h>
+#include <libyul/backends/evm/EVMDialect.h>
+
+#include <libevmasm/SemanticInformation.h>
+
+#include <libdevcore/CommonData.h>
+
+using namespace std;
+using namespace dev;
+using namespace yul;
+
+MovableChecker::MovableChecker(Dialect const& _dialect):
+	m_dialect(_dialect)
+{
+}
+
+MovableChecker::MovableChecker(Dialect const& _dialect, Expression const& _expression):
+	MovableChecker(_dialect)
+{
+	visit(_expression);
+}
+
+void MovableChecker::operator()(Identifier const& _identifier)
+{
+	ASTWalker::operator()(_identifier);
+	m_variableReferences.emplace(_identifier.name);
+}
+
+void MovableChecker::operator()(FunctionalInstruction const& _instr)
+{
+	ASTWalker::operator()(_instr);
+
+	if (!eth::SemanticInformation::movable(_instr.instruction))
+		m_movable = false;
+	if (!eth::SemanticInformation::sideEffectFree(_instr.instruction))
+		m_sideEffectFree = false;
+}
+
+void MovableChecker::operator()(FunctionCall const& _functionCall)
+{
+	ASTWalker::operator()(_functionCall);
+
+	if (BuiltinFunction const* f = m_dialect.builtin(_functionCall.functionName.name))
+	{
+		if (!f->movable)
+			m_movable = false;
+		if (!f->sideEffectFree)
+			m_sideEffectFree = false;
+	}
+	else
+	{
+		m_movable = false;
+		m_sideEffectFree = false;
+	}
+}
+
+void MovableChecker::visit(Statement const&)
+{
+	assertThrow(false, OptimizerException, "Movability for statement requested.");
+}
+
+pair<TerminationFinder::ControlFlow, size_t> TerminationFinder::firstUnconditionalControlFlowChange(
+	vector<Statement> const& _statements
+)
+{
+	for (size_t i = 0; i < _statements.size(); ++i)
+	{
+		ControlFlow controlFlow = controlFlowKind(_statements[i]);
+		if (controlFlow != ControlFlow::FlowOut)
+			return {controlFlow, i};
+	}
+	return {ControlFlow::FlowOut, size_t(-1)};
+}
+
+TerminationFinder::ControlFlow TerminationFinder::controlFlowKind(Statement const& _statement)
+{
+	if (
+		_statement.type() == typeid(ExpressionStatement) &&
+		isTerminatingBuiltin(boost::get<ExpressionStatement>(_statement))
+	)
+		return ControlFlow::Terminate;
+	else if (_statement.type() == typeid(Break))
+		return ControlFlow::Break;
+	else if (_statement.type() == typeid(Continue))
+		return ControlFlow::Continue;
+	else
+		return ControlFlow::FlowOut;
+}
+
+bool TerminationFinder::isTerminatingBuiltin(ExpressionStatement const& _exprStmnt)
+{
+	if (_exprStmnt.expression.type() == typeid(FunctionalInstruction))
+		return eth::SemanticInformation::terminatesControlFlow(
+			boost::get<FunctionalInstruction>(_exprStmnt.expression).instruction
+		);
+	else if (_exprStmnt.expression.type() == typeid(FunctionCall))
+		if (auto const* dialect = dynamic_cast<EVMDialect const*>(&m_dialect))
+			if (auto const* builtin = dialect->builtin(boost::get<FunctionCall>(_exprStmnt.expression).functionName.name))
+				if (builtin->instruction)
+					return eth::SemanticInformation::terminatesControlFlow(*builtin->instruction);
+	return false;
+}
---
+++ libyul/optimiser/Semantics.h
@@ -0,0 +1,99 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * Specific AST walkers that collect semantical facts.
+ */
+
+#pragma once
+
+#include <libyul/optimiser/ASTWalker.h>
+
+#include <set>
+
+namespace yul
+{
+struct Dialect;
+
+/**
+ * Specific AST walker that determines whether an expression is movable.
+ */
+class MovableChecker: public ASTWalker
+{
+public:
+	explicit MovableChecker(Dialect const& _dialect);
+	MovableChecker(Dialect const& _dialect, Expression const& _expression);
+
+	void operator()(Identifier const& _identifier) override;
+	void operator()(FunctionalInstruction const& _functionalInstruction) override;
+	void operator()(FunctionCall const& _functionCall) override;
+
+	/// Disallow visiting anything apart from Expressions (this throws).
+	void visit(Statement const&) override;
+	using ASTWalker::visit;
+
+	bool movable() const { return m_movable; }
+	bool sideEffectFree() const { return m_sideEffectFree; }
+
+	std::set<YulString> const& referencedVariables() const { return m_variableReferences; }
+
+private:
+	Dialect const& m_dialect;
+	/// Which variables the current expression references.
+	std::set<YulString> m_variableReferences;
+	/// Is the current expression movable or not.
+	bool m_movable = true;
+	/// Is the current expression side-effect free, i.e. can be removed
+	/// without changing the semantics.
+	bool m_sideEffectFree = true;
+};
+
+/**
+ * Helper class to find "irregular" control flow.
+ * This includes termination, break and continue.
+ */
+class TerminationFinder
+{
+public:
+	enum class ControlFlow { FlowOut, Break, Continue, Terminate };
+
+	TerminationFinder(Dialect const& _dialect): m_dialect(_dialect) {}
+
+	/// @returns the index of the first statement in the provided sequence
+	/// that is an unconditional ``break``, ``continue`` or a
+	/// call to a terminating builtin function.
+	/// If control flow can continue at the end of the list,
+	/// returns `FlowOut` and ``size_t(-1)``.
+	/// The function might return ``FlowOut`` even though control
+	/// flow cannot actually continue.
+	std::pair<ControlFlow, size_t> firstUnconditionalControlFlowChange(
+		std::vector<Statement> const& _statements
+	);
+
+	/// @returns the control flow type of the given statement.
+	/// This function could return FlowOut even if control flow never continues.
+	ControlFlow controlFlowKind(Statement const& _statement);
+
+	/// @returns true if the expression statement is a direct
+	/// call to a builtin terminating function like
+	/// ``stop``, ``revert`` or ``return``.
+	bool isTerminatingBuiltin(ExpressionStatement const& _exprStmnt);
+
+private:
+	Dialect const& m_dialect;
+};
+
+}
