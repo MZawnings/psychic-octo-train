8e65f162b99cfd7eb946f3ec1d68dc337037e45c
MZ_Moola <96755127+MZawnings@users.noreply.github.com>
2023-04-17 21:12:22 +0200

    Initialize fix-9676-refactoring

---
+++ test/tools/ossfuzz/solc_noopt_ossfuzz.cpp
@@ -0,0 +1,60 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// SPDX-License-Identifier: GPL-3.0
+
+#include <test/tools/fuzzer_common.h>
+
+#include <test/TestCaseReader.h>
+
+#include <sstream>
+
+using namespace solidity::frontend::test;
+using namespace std;
+
+// Prototype as we can't use the FuzzerInterface.h header.
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const* _data, size_t _size);
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const* _data, size_t _size)
+{
+	if (_size <= 10240)
+	{
+		string input(reinterpret_cast<char const*>(_data), _size);
+		map<string, string> sourceCode;
+		try
+		{
+			TestCaseReader t = TestCaseReader(std::istringstream(input));
+			sourceCode = t.sources().sources;
+		}
+		catch (runtime_error const&)
+		{
+			return 0;
+		}
+		auto e = FuzzerUtil::testCompiler(
+			sourceCode,
+			/*optimize=*/false,
+			/*_rand=*/static_cast<unsigned>(_size),
+			/*forceSMT=*/true
+		);
+		if (e == FuzzerUtil::Error::SUCCESS)
+			std::cout << "Compiler: Success" << std::endl;
+		else if (e == FuzzerUtil::Error::FAILURE)
+			std::cout << "Compiler: Failure" << std::endl;
+		else
+			std::cout << "Compiler: Exception" << std::endl;
+	}
+	return 0;
+}
