48fe1107c8db35c21d08bc1d0d77e8f171f7719a
MZ_Moola <96755127+MZawnings@users.noreply.github.com>
2023-04-17 21:42:45 +0200

    Initialize memcopy-precompile

---
+++ test/libsolidity/semanticTests/constructor/bytes_in_constructors_unpacker.sol
@@ -0,0 +1,17 @@
+contract Test {
+    uint public m_x;
+    bytes public m_s;
+    constructor(uint x, bytes memory s) {
+        m_x = x;
+        m_s = s;
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// constructor(): 7, 0x40, 78, "abcdefghijklmnopqrstuvwxyzabcdef", "ghijklmnopqrstuvwxyzabcdefghijkl", "mnopqrstuvwxyz" ->
+// gas irOptimized: 273220
+// gas legacy: 300898
+// gas legacyOptimized: 252010
+// m_x() -> 7
+// m_s() -> 0x20, 78, "abcdefghijklmnopqrstuvwxyzabcdef", "ghijklmnopqrstuvwxyzabcdefghijkl", "mnopqrstuvwxyz"
