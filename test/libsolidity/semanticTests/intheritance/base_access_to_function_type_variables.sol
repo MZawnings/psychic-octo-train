e64e0d0504a7c2161a6f9b5723e18f3c71e650a7
MZ_Moola <96755127+MZawnings@users.noreply.github.com>
2023-04-17 21:10:27 +0200

    Initialize readFromStorageArraySol2Yul

---
+++ test/libsolidity/semanticTests/intheritance/base_access_to_function_type_variables.sol
@@ -0,0 +1,24 @@
+contract C {
+    function() returns (uint256) internal x;
+
+    function set() public {
+        C.x = g;
+    }
+
+    function g() public pure returns (uint256) {
+        return 2;
+    }
+
+    function h() public returns (uint256) {
+        return C.x();
+    }
+}
+
+// ====
+// compileViaYul: also
+// compileToEwasm: also
+// ----
+// g() -> 2
+// h() -> FAILURE
+// set() ->
+// h() -> 2
