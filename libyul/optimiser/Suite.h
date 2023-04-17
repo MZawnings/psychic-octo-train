db26eae652945a1f0696bc2d612bf1bf1f917ebb
MZ_Moola <96755127+MZawnings@users.noreply.github.com>
2023-04-17 21:02:35 +0200

    Initialize abiv2fuzzer-with-holes

---
+++ libyul/optimiser/Suite.h
@@ -0,0 +1,53 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * Optimiser suite that combines all steps and also provides the settings for the heuristics.
+ */
+
+#pragma once
+
+#include <libyul/AsmDataForward.h>
+#include <libyul/YulString.h>
+#include <liblangutil/EVMVersion.h>
+
+#include <set>
+
+namespace yul
+{
+
+struct AsmAnalysisInfo;
+struct Dialect;
+class GasMeter;
+struct Object;
+
+/**
+ * Optimiser suite that combines all steps and also provides the settings for the heuristics.
+ * Only optimizes the code of the provided object, does not descend into the sub-objects.
+ */
+class OptimiserSuite
+{
+public:
+	static void run(
+		Dialect const& _dialect,
+		GasMeter const* _meter,
+		Object& _object,
+		bool _optimizeStackAllocation,
+		std::set<YulString> const& _externallyUsedIdentifiers = {}
+	);
+};
+
+}
